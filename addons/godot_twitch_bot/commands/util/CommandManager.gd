b0e60c009da361686213fa97995828117ea55903
SilicDev <83308290+SilicDev@users.noreply.github.com>
2022-09-25 15:22:03 +0200

    Add random command variable

--- addons/godot_twitch_bot/commands/util/CommandManager.gd
+++ addons/godot_twitch_bot/commands/util/CommandManager.gd
@@ -35,8 +35,22 @@ func get_response(cmd: String, message: Dictionary) -> String:
 		msg = msg.replace("${sender}", message.tags["display-name"])
 		msg = msg.replace("${touser}", params[0] if not params.empty() else message.tags["display-name"])
 		msg = msg.replace("${channel}", channel)
+		msg = handle_random(msg)
 		return msg
 	return ""
+
+
+func handle_random(msg: String) -> String:
+	var regex = RegEx.new()
+	regex.compile("\\$\\{random.([0-9]{1,3})\\}")
+	var matches = regex.search_all(msg)
+	for m in matches:
+		var matched = m.strings[0]
+		var bound = int(matched.substr(9, matched.length() - 10))
+		var pos = msg.find(matched)
+		msg.erase(pos, matched.length())
+		msg = msg.insert(pos, str(randi() % bound + 1))
+	return msg
 
 
 func load_command_list(path: String):
