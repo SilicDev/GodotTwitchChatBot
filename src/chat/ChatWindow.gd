a4af3798cd0e49a7b8a155f68b4b3766d45a4a25
SilicDev <83308290+SilicDev@users.noreply.github.com>
2022-10-02 23:06:17 +0200

    Fix panelcontainer channel folder being generated

--- src/chat/ChatWindow.gd
+++ src/chat/ChatWindow.gd
@@ -37,7 +37,6 @@ onready var configMenu := $ChannelConfigDialog
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	load_ini()
 	pass # Replace with function body.
 
 
