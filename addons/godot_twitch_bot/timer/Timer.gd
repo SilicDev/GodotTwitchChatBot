c87bedb9b09070a0e6fe39a9f8db57bc32289183
SilicDev <83308290+SilicDev@users.noreply.github.com>
2022-10-17 22:43:24 +0200

    Add utility to Timers

--- addons/godot_twitch_bot/timer/Timer.gd
+++ addons/godot_twitch_bot/timer/Timer.gd
@@ -1,6 +1,9 @@
 extends Reference
 
 
+signal timer_triggered(message)
+
+
 var messages := {}
 
 var cooldown := 30
@@ -8,3 +11,10 @@ var min_messages := 15
 
 var message_counter := 0
 var last_sent := Time.get_ticks_msec() - (cooldown / 2 * 60 * 1000)
+
+var active := false
+
+
+func update() -> void:
+	message_counter += 1
+	
